1998a910d626f3869b64c64526a9a092f34eab5f
Matt Jones <gitcode@magisa.org>
2013-10-28 23:50:11 -0800

    Added script to export the zone file as a BIND compliant file.  Closes #3.

---
+++ export.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+#
+# Matt Jones 2013
+#
+# Export a zone's records in a standard BIND zone format
+# Usage: ./export.sh <mydomain.org>
+
+. ./rsdns_functions.sh
+
+DOMAIN=$1
+TMP=status.xml
+CRED=credentials
+NS2=http://docs.rackspacecloud.com/dns/api/v1.0
+. ./$CRED 
+
+getDomainId
+
+curl  -s \
+-H "X-Auth-Token: $TOKEN"  \
+-H 'Accept: application/xml'  \
+"https://dns.api.rackspacecloud.com/v1.0/$TENANT/domains/$DOMAINID/export" | xmlstarlet fo > $TMP
+CALLBACK=`xmlstarlet sel -N ns2=$NS2 -T -t -m "/ns2:asyncResponse" -v "ns2:callbackUrl" --nl $TMP`
+STATUS=`xmlstarlet sel -N ns2=$NS2 -T -t -m "/ns2:asyncResponse" -v "ns2:status" --nl $TMP`
+rm $TMP
+
+while [ "$STATUS" != "COMPLETED" ]; do
+	curl -s -H "X-Auth-Token: $TOKEN" -H 'Accept: application/xml' "${CALLBACK}?showDetails=true" | xmlstarlet fo > $TMP
+	STATUS=`xmlstarlet sel -N ns2=$NS2 -T -t -m "/ns2:asyncResponse" -v "ns2:status" --nl $TMP`
+done
+
+xmlstarlet sel -N ns2=$NS2 -T -t -m "/ns2:asyncResponse" -v "ns2:response/ns2:contents" --nl $TMP
